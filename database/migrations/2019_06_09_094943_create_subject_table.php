ec54601dbdb6e5fba710a4d614455fc005e5ee9b
valonfejzullahu <valonfejzullahu@users.noreply.github.com>
2019-06-09 14:12:59 +0200

    Main tables added

---
+++ database/migrations/2019_06_09_094943_create_subject_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string("name", 255);
+            $table->bigInteger("professor");
+            $table->bigInteger("student");
+            $table->integer("department");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject');
+    }
+}
